02d9b04540f7d4f7804aa8cdb5c61ac38f3684ff
sridevidoraraj <sridevi.elangovan@gcitsolutions.com>
2022-07-06 18:58:13 +0530

    dayFourTask modified

---
+++ src/com/gcit/dayFour/DefaultCase.java
@@ -0,0 +1,26 @@
+package com.gcit.dayFour;
+//9. default case in Java switch-case
+public class DefaultCase {
+    public static void main(String args[]){
+        char grade = 'E';
+        switch(grade){
+            case 'A':
+                System.out.println("excellent");
+                break;
+            case 'B':
+                System.out.println("good");
+                break;
+            case 'c':
+                System.out.println("improve");
+                break;
+            case 'D':
+                System.out.println("fail");
+                break;
+            default:
+                System.out.println("invalid grade");
+                break;
+        }
+        System.out.println("ur grade is : " +grade);
+
+    }
+}
