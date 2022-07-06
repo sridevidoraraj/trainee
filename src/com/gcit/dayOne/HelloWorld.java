a8e2e95203f2b8218343ec9cc26a429e289d73d0
sridevidoraraj <sridevi.elangovan@gcitsolutions.com>
2022-07-06 14:27:32 +0530

    dayThreeTask modified

--- src/com/gcit/dayOne/HelloWorld.java
+++ src/com/gcit/dayOne/HelloWorld.java
@@ -1,7 +1,8 @@
 package com.gcit.dayOne;
 
 public class HelloWorld {
-    public static void main(String args[]){
+    public static void main(String args[])
+    {
         System.out.println("hello world");
     }
 }
